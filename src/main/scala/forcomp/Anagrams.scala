860147f9a820ae4c5a9dcf641fe22ef9eb5df46c
Gaurav Gaur <vaibgaurav@gmail.com>
2018-02-11 21:24:04 +0000

    subtract elements

--- src/main/scala/forcomp/Anagrams.scala
+++ src/main/scala/forcomp/Anagrams.scala
@@ -126,7 +126,19 @@ object Anagrams {
     * Note: the resulting value is an occurrence - meaning it is sorted
     * and has no zero-entries.
     */
-  def subtract(x: Occurrences, y: Occurrences): Occurrences = ???
+  def subtract(x: Occurrences, y: Occurrences): Occurrences = {
+    val mapX = x.toMap
+    val mapY = y.toMap
+
+    val list = mapX.map { case(k,xv) =>
+      mapY.get(k) match {
+        case None => k -> xv
+        case Some(yv) => k -> (xv-yv)
+      }
+    }.toList
+
+    list.filter(_._2 != 0)
+  }
 
   /** Returns a list of all anagram sentences of the given sentence.
     *
