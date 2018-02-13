59011a1ba744519da7d708ac62658a7a12b5ba50
Gaurav Gaur <gaurav.gaur@digital.hmrc.gov.uk>
2018-02-13 14:20:31 +0000

    added another version of combinations

--- src/main/scala/forcomp/Combination.scala
+++ src/main/scala/forcomp/Combination.scala
@@ -8,10 +8,10 @@ object Combination {
     def c(in: List[Int]): Seq[List[Int]] = {
       in match {
         case Nil => List(Nil)
-        case h::tail =>
+        case h :: tail =>
 
           val o1: Seq[List[Int]] = tail map { t =>
-            List(h,t)
+            List(h, t)
           }
 
           val o2 = c(tail)
@@ -24,7 +24,39 @@ object Combination {
       }
     }
 
-    c(in).sortWith(_.length<_.length)
+    c(in).sortWith(_.length < _.length)
+  }
+
+  def c1(in: List[(Char, Int)]): Seq[(Char, Int)] = in flatMap { l => (1 to l._2) map ((l._1, _)) }
+
+  type CI = (Char, Int)
+
+  def combGen(in: List[CI]): List[List[CI]] = {
+
+
+    def c(in: List[CI]): List[List[CI]] = {
+      in match {
+        case Nil => List(Nil)
+        case h :: tail =>
+
+          val o1: Seq[List[CI]] = tail map { t =>
+            if (h._1 != t._1) List(h, t) else Nil
+          }
+
+          val o2 = c(tail)
+
+          val o: Seq[List[CI]] = o2 map { x =>
+            if (x.unzip._1.contains(h._1))
+              x
+            else
+              h :: x
+          }
+
+          (o ++ o1 ++ o2).toSet.toList
+      }
+    }
+
+    c(in)
   }
 
 }
--- src/test/scala/forcomp/CombinationSuite.scala
+++ src/test/scala/forcomp/CombinationSuite.scala
@@ -16,4 +16,31 @@ class CombinationSuite extends FunSuite  {
     assert(combinations(List(1,2,3)) == List(List(), List(1), List(3), List(2), List(1, 2), List(2, 3), List(1, 3), List(1, 2, 3)))
   }
 
+  test("3") {
+    assert(c1(List(('a', 2))) == List[(Char, Int)](('a', 1),('a', 2)))
+  }
+
+  test("4") {
+    assert(c1(List(('a', 2), ('b', 2))) == List[(Char, Int)](('a', 1),('a', 2), ('b', 1), ('b', 2)))
+  }
+
+  test("combinations: abba") {
+    val abba = List(('a', 2), ('b', 2))
+    val abbacomb = List(
+      List(),
+      List(('a', 1)),
+      List(('a', 2)),
+      List(('b', 1)),
+      List(('a', 1), ('b', 1)),
+      List(('a', 2), ('b', 1)),
+      List(('b', 2)),
+      List(('a', 1), ('b', 2)),
+      List(('a', 2), ('b', 2))
+    )
+    val abbaC1: List[(Char, Int)] = c1(abba).toList
+    val result = combGen(abbaC1).sortWith(_.length < _.length)
+    println(result.length)
+    assert(result.toSet === abbacomb.toSet)
+  }
+
 }
